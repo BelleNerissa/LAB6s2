948d0b638053ca3df9b54af4ddb714d34474ba31
BelleNerissa <49173787+BelleNerissa@users.noreply.github.com>
2023-03-22 13:13:01 -0300

    commit final

--- data/query_repo.py
+++ data/query_repo.py
@@ -1,15 +1,15 @@
 from os import environ
 import os
-
+from dotenv import load_dotenv
 import requests
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
 
 MAX_QUERY_ATTEMPTS = 10
 AFTER_PREFIX = ', after: {cursor}'
 OUTPUT = 'data/repositories.csv'
 
-os.environ['GITHUB_TOKEN'] = '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'
-
-# We choose the first 25 repositories to avoid 502 errors from GitHub GraphQL API.
 QUERY = """
     {
       search(query: "language:java,stars:>100 sort:stars created:{interval}", type: REPOSITORY, first: 25 {after}) {
@@ -37,78 +37,79 @@ QUERY = """
     }
     """
 
+# Exporta a lista de repositórios em um arquivo CSV..
 
-def export_csv(repos: list) -> None:
-    """
-    This function exports the list of repositories to a CSV file.
-    """
+
+def export_csv(repos: list) -> str:
     with open(OUTPUT, 'w') as f:
         f.write('nameWithOwner,url,createdAt,stargazers,releases\n')
         for repo in repos:
-            f.write('{},{},{},{},{}\n'.format(
-                repo['nameWithOwner'],
-                repo['url'],
-                repo['createdAt'],
-                repo['stargazers']['totalCount'],
-                repo['releases']['totalCount']
-            ))
+            f.write(
+                f"{repo['nameWithOwner']},{repo['url']},{repo['createdAt']},{repo['stargazers']['totalCount']},{repo['releases']['totalCount']}\n")
     return OUTPUT
 
+ # Executa uma query na API GraphQL do GitHub e retorna o resultado.
 
-def query_runner(query: str, token: str, attemp=1) -> dict:
-    """
-    This function runs a query against the GitHub GraphQL API and returns the result.
-    """
+# Executa uma query na API GraphQL do GitHub e retorna o resultado.
+
+
+def run_graphql_query(query: str, token: str, attemp=1) -> dict:
+
     url = 'https://api.github.com/graphql'
-    headers = {'Authorization': 'Bearer {}'.format(token)}
+    headers = {'Authorization': f'Bearer {token}'}
     response = requests.post(url, json={'query': query}, headers=headers)
     if response.status_code == 200:
         return response.json()
     elif response.status_code == 502 and attemp <= MAX_QUERY_ATTEMPTS:
-        print('Attemp {}/{} get Error 502. Retrying...'.format(attemp, MAX_QUERY_ATTEMPTS))
-        return query_runner(query, token, attemp + 1)
+        print(
+            f'Attempt {attemp}/{MAX_QUERY_ATTEMPTS} got Error 502. Retrying...')
+        return run_graphql_query(query, token, attemp + 1)
     elif response.status_code == 502 and attemp > MAX_QUERY_ATTEMPTS:
-        print('Error 502. Maximum number of attempts reached. Try again later.')
+        print('Error 502. Maximum number of attempts reached.')
         exit(1)
     else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, query))
+        raise Exception(
+            f"Query failed to run by returning code of {response.status_code}. {query}")
 
 
+# Retorna uma lista dos repositórios mais populares no GitHub e suas características.
 def get_repos(token: str, interval: str, after: str) -> list:
-    """
-    This function returns a list of the most popular repositories on GitHub and their caracteristics.
-    """
     query = QUERY.replace('{after}', after)
     query = query.replace('{interval}', interval)
-    result = query_runner(query, token)
+    result = run_graphql_query(query, token)
 
     if 'data' in result:
         return result['data']
     else:
         print(result)
-        raise Exception('Error getting repositories. Message: {}. \n {}'.format(result['message'], query))
+        raise Exception(
+            f"Error getting repositories. Message: {result['message']}. \n {query}")
 
 
-def generate_repo_csv(results: int, token: str=None) -> str:
-    if not token:
-        if 'GITHUB_TOKEN' in environ:
-            token = environ['GITHUB_TOKEN']
-        else:
+# Gera o csv dos repositorios minerados
+def mine_github_repositories_csv(results: int, token: str = None) -> str:
+    if token is None:
+        # Usa as variáveis de ambiente definidas no arquivo .env
+        token = os.environ.get('GITHUB_TOKEN')
+        if token is None:
             raise Exception(
-                "You need to set the GITHUB_TOKEN environment variable or pass your token as an argument")
+                "Você precisa definir a variável de ambiente GITHUB_TOKEN de tipo clássico")
 
-    after = '' # Pagination
-    interval = '2008..2015' # Year interval
-    repositories = [] # List of repositories
-    while (len(repositories) < (results)):
+    after = ''
+    interval = '2008..2015'
+    repositories = []
+
+    while len(repositories) < results:
         response = get_repos(token, interval, after)
-        repositories.extend(response['search']['nodes'])
-        if response['search']['pageInfo']['hasNextPage']:
+        nodes = response.get('search', {}).get('nodes', [])
+        repositories.extend(nodes)
+
+        if response.get('search', {}).get('pageInfo', {}).get('hasNextPage'):
             cursor = response['search']['pageInfo']['endCursor']
             after = AFTER_PREFIX.format(cursor=f'"{cursor}"')
         elif len(repositories) <= results:
             after = ''
-            interval = '2016..2022' # New interval
+            interval = '2016..2023'
         else:
             break
 
@@ -117,10 +118,17 @@ def generate_repo_csv(results: int, token: str=None) -> str:
 
 if __name__ == '__main__':
     import argparse
+    # Criar objeto ArgumentParser
     parser = argparse.ArgumentParser(
-        description='Query the GitHub GraphQL API to get most popular repositories.')
+        description='Consulte a API GitHub GraphQL para obter os repositórios mais populares.')
+
+    # Adicionar argumentos
     parser.add_argument('--token', '-t', help='GitHub access token')
-    parser.add_argument('--results', '-r', help='Number of results to return', type=int, default=5)
+    parser.add_argument(
+        '--results', '-r', help='Number of results to return', type=int, default=100)
+
+    # Fazer o parse dos argumentos da linha de comando
     args = parser.parse_args()
 
-    generate_repo_csv(args.results, args.token)
+    # Chamar a função com os argumentos obtidos
+    mine_github_repositories_csv(args.results, args.token)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os, shutil, subprocess
+import os
+import shutil
+import subprocess
 from datetime import datetime as dt
 import multiprocessing as mul
 import os
@@ -8,9 +10,9 @@ from subprocess import call
 import pandas as pd
 from git import Repo
 
-from data.query_repo import generate_repo_csv
+from data.query_repo import mine_github_repositories_csv
 
-NUM_REPO = 3
+NUM_REPO = 1000
 
 REPOS_FOLDER = 'data/repos/'
 METRICS_FOLDER = 'data/ck_metrics/'
@@ -21,63 +23,71 @@ INPUT_FILE = 'data/repositories.csv'
 OUTPUT = 'output/analysis.csv'
 
 COLUMNS = ['nameWithOwner', 'url', 'createdAt', 'stargazers',
-            'releases', 'loc', 'cbo', 'dit', 'lcom']
+           'releases', 'loc', 'cbo', 'dit', 'lcom']
 
 
 def log_print(message):
-    now = dt.now().strftime('%Y-%m-%d %H:%M:%S')
-    print(f'{now} - {message}')
+    nowDatetime = dt.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(f'{nowDatetime} - {message}')
 
 
+# Verifica se o repositório já foi analisado comparando pela coluna nameWithOwner
 def already_processed(nameWithOwner: str) -> bool:
     if os.path.exists(OUTPUT):
         df = pd.read_csv(OUTPUT)
         return nameWithOwner in df['nameWithOwner'].values
     return False
 
+# Função para mudar as permissões de escrita do arquivo ou diretório especificado pelo caminho path e, em seguida, tenta remove-lo.
+
+
 def on_rm_error(func, path, exc_info):
     os.chmod(path, stat.S_IWRITE)
     os.unlink(path)
 
 
-def delete_cached_repos(repo_name: str):
+# Função que exclui os dados armazenados do clone do repositório
+def delete_cached_repo_data(repo_name: str):
     try:
-        if os.path.exists(REPOS_FOLDER + repo_name):
+        if os.path.exists(os.path.join(REPOS_FOLDER, repo_name)):
+            with os.scandir(os.path.join(REPOS_FOLDER, repo_name)) as entries:
+                for entry in entries:
+                    if entry.name.endswith(('git', 'github')):
+                        tmp = os.path.join(REPOS_FOLDER, repo_name, entry.name)
+                        # Queremos exibir a pasta .git antes de desvinculá-la.
+                        with subprocess.Popen(['attrib', '-H', tmp], stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
+                            stdout, stderr = proc.communicate()
+                        # Para caso exista arquivos que não permitem exclusão, como exemplo '.git'
+                        shutil.rmtree(tmp, onerror=on_rm_error)
+            shutil.rmtree(os.path.join(REPOS_FOLDER, repo_name))
 
-            for i in os.listdir(REPOS_FOLDER + repo_name):
-                if i.endswith('git') or i.endswith('github'):
-                    tmp = os.path.join(REPOS_FOLDER + repo_name, i)
-                    # We want to unhide the .git folder before unlinking it.
-                    while True:
-                        call(['attrib', '-H', tmp])
-                        break
-                    shutil.rmtree(tmp, onerror=on_rm_error)
-            shutil.rmtree(REPOS_FOLDER + repo_name) 
-
-        if os.path.exists(METRICS_FOLDER + repo_name + 'class.csv'):
-            os.remove(METRICS_FOLDER + repo_name + 'class.csv')
-        if os.path.exists(METRICS_FOLDER + repo_name + 'method.csv'):
-            os.remove(METRICS_FOLDER + repo_name + 'method.csv')
+        for file_name in ['class.csv', 'method.csv']:
+            file_path = os.path.join(METRICS_FOLDER, repo_name + file_name)
+            if os.path.exists(file_path):
+                os.remove(file_path)
     except Exception as e:
         log_print(e)
         log_print(f'Error on exclude {repo_name} =/ ')
+
+# Função que executa a ferramenta de qualidade de codigo CK (https://github.com/mauricioaniche/ck)
 
 
 def run_ck_metrics(nameWithOwner: str, url: str, created_at: str, stargazers: int, releases: int) -> None:
     try:
         repo_name = nameWithOwner.replace('/', '@')
-        delete_cached_repos(repo_name)
+        delete_cached_repo_data(repo_name)
 
-        # Assert that the repository isn't already in the output
+        # Verifica se o repositório ainda não teve sua análise realizada
         if not os.path.exists(f'{OUTPUT_FOLDER}{repo_name}.csv') and not already_processed(nameWithOwner):
             repo_path = REPOS_FOLDER + repo_name
             ck_metrics_path = METRICS_FOLDER + repo_name
 
-            log_print(f'Cloning {nameWithOwner}')
+            log_print('Cloning {}'.format(nameWithOwner))
             Repo.clone_from(url, repo_path, depth=1, filter='blob:none')
 
-            log_print(f'Running CK metrics on {repo_name}')
-            subprocess.call(["java", "-jar", CK_RUNNER, repo_path, "true", "0", "False", ck_metrics_path])
+            log_print('Running CK metrics on {}'.format(repo_name))
+            subprocess.call(["java", "-jar", CK_RUNNER, repo_path,
+                             "true", "0", "False", ck_metrics_path])
 
             metrics = pd.read_csv(ck_metrics_path + 'class.csv')
             loc = metrics['loc'].sum()
@@ -86,7 +96,7 @@ def run_ck_metrics(nameWithOwner: str, url: str, created_at: str, stargazers: in
             lcom = metrics['lcom'].median()
 
             df = pd.DataFrame([[nameWithOwner, url, created_at, stargazers,
-                releases, loc, cbo, dit, lcom]], columns=COLUMNS)
+                                releases, loc, cbo, dit, lcom]], columns=COLUMNS)
             df.to_csv(OUTPUT_FOLDER + repo_name + '.csv', index=False)
 
             shutil.rmtree(repo_path)
@@ -94,59 +104,62 @@ def run_ck_metrics(nameWithOwner: str, url: str, created_at: str, stargazers: in
             os.remove(ck_metrics_path + 'method.csv')
 
             return df
-    
+
     except Exception as e:
-        log_print(f'Error on {nameWithOwner}: {e}')
-        delete_cached_repos(repo_name)
+        log_print('Error on {}: {}'.format(nameWithOwner, e))
+        delete_cached_repo_data(repo_name)
 
 
 if __name__ == '__main__':
     rp_list = pd.DataFrame()
 
-    # Create folders if they don't exist
-    if not os.path.exists(OUTPUT_FOLDER): os.makedirs(OUTPUT_FOLDER)
-    if not os.path.exists(REPOS_FOLDER): os.makedirs(REPOS_FOLDER)
-    if not os.path.exists(METRICS_FOLDER): os.makedirs(METRICS_FOLDER)
+    # Crie pastas se elas não existirem
+    if not os.path.exists(OUTPUT_FOLDER):
+        os.makedirs(OUTPUT_FOLDER)
+    if not os.path.exists(REPOS_FOLDER):
+        os.makedirs(REPOS_FOLDER)
+    if not os.path.exists(METRICS_FOLDER):
+        os.makedirs(METRICS_FOLDER)
 
-    # Verify if the input file exists. If not, generate it.
-    #if os.path.exists(INPUT_FILE):
-        #log_print('Reading repositories')
-    #else:
-        #log_print('Generating repositories.csv')
-        #generate_repo_csv(NUM_REPO)
+    # Verifica se o arquivo de entrada existe e se a entrada é válida. Se não, gera-o.
+    if os.path.exists(INPUT_FILE):
+        rp_list = pd.read_csv(INPUT_FILE)
+        log_print('Reading repositories')
+    else:
+        log_print('Generating new repositories.csv')
+        mine_github_repositories_csv(NUM_REPO)
+        rp_list = pd.read_csv(INPUT_FILE)
+        log_print('Generating repositories.csv')
 
-    # Read the input file
-    rp_list = pd.read_csv(INPUT_FILE)
+    # Ordena a lista por estrelas
+    rp_list = rp_list.sort_values(by='stargazers', ascending=False)[
+        :int(NUM_REPO*1.25)]
 
-    # Verify if the input file is valid. If not, generate it.
-    #if len(rp_list) <= NUM_REPO:
-        #log_print('Generating new repositories.csv')
-        #generate_repo_csv(NUM_REPO)
-
-    rp_list = rp_list.sort_values(by='stargazers', ascending=False)[:int(NUM_REPO*1.25)]
-    # Declare the number of threads to use
+    # Declara o número de threads a serem utilizadas
     pool = mul.Pool(int(os.cpu_count())*3)
 
-    # Get the input file and filter out the already read repositories
+    # Obtenha o arquivo de entrada e filtre os repositórios já lidos
     rp_list = pd.read_csv(INPUT_FILE)
 
-    # Run the CK metrics for each repository
+    # Execute as métricas CK para cada repositório
     rows = list(rp_list.itertuples(name=None, index=False))
     results = pool.starmap(run_ck_metrics, rows)
     results = list(filter(lambda x: x is not None, results))
 
-    # Build the output file for each repository analyzed in outut folder and remove
-    output = pd.concat(results) if len(results) > 0 else pd.DataFrame(columns=COLUMNS)
+    # Cria o arquivo de saída para cada repositório analisado na pasta de saída e remove-o
+    output = pd.concat(results) if len(
+        results) > 0 else pd.DataFrame(columns=COLUMNS)
     if os.path.exists(OUTPUT):
         output = pd.concat([output, pd.read_csv(OUTPUT)])
     for repo in os.listdir(OUTPUT_FOLDER):
         output = pd.concat([output, pd.read_csv(OUTPUT_FOLDER + repo)])
         os.remove(OUTPUT_FOLDER + repo)
 
-    # Assert that the output file has not duplicates
+    # Verifica se o arquivo de saída não tem duplicatas
     output = output.drop_duplicates()
     output = output.sort_values(by='stargazers', ascending=False)
-    # Export the output file
+
+    # Exporta o arquivo csv
     output.to_csv(OUTPUT, index=False)
 
     log_print('Done')
